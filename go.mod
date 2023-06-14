a30c345064100d5e7333d867d95db63de63a819d
Himanshu Kumar Singh <97310760+to4to@users.noreply.github.com>
2023-06-15 02:30:19 +0530

    Mongo Package Added + Started userModel

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/to4to/Go-JWT-Auth
 
 go 1.20
+
+require go.mongodb.org/mongo-driver v1.11.7 // indirect
--- models/userModel.go
+++ models/userModel.go
@@ -1,9 +1,22 @@
 package models
 
-import()
+import (
+	"time"
 
-type User struct{
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
+type User struct {
+	ID            primitive.ObjectID `bson:"id"`
+	First_name    *string            `json:"first_name" validate:"required",min=2,max=100`
+	Last_name     *string
+	Password      *string
+	Email         *string
+	Phone         *string
+	Token         *string
+	User_type     *string
+	Refresh_token *string
+	Created_at    time.Time
+	Updated_at    time.Time
+	User_id       string
 }
-
-
