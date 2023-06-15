f424b9059f13f3b39ed073cd34bea01aeeb299d4
Himanshu Kumar Singh <97310760+to4to@users.noreply.github.com>
2023-06-16 01:13:01 +0530

    router for api1 & api2 added

--- main.go
+++ main.go
@@ -1,23 +1,38 @@
 package main
-import(
+
+import (
 	"os"
 
-	
+	"github.com/gin-gonic/gin"
+	"github.com/to4to/Go-JWT-Auth/routes"
+)
 
-"github.com/gin-gonic/gin")
+func main() {
 
-func main(){
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	routes.AuthRoutes(router)
+	routes.UserRoutes(router)
+
+	router.GET("/api-1", func(c *gin.Context) {
+
+		c.JSON(200, gin.H{"success": "Access Granted for Api-1"})
+
+	})
+
+	router.GET("/api-2", func(c *gin.Context) {
+
+		c.JSON(200, gin.H{"success": "Access Granted for Api-2"})
+
+	})
 
 
-port:=os.Getenv("PORT")
-
-if port==""{
-	port="8000"
-}
-
-
-router:=gin.New()
-
-router.Use(gin.Logger())
-	
+	router.Run(":"+port)
 }
