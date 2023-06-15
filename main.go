e5650ff79bf307d365f4e170d78586e84fd10ca1
Himanshu Kumar Singh <97310760+to4to@users.noreply.github.com>
2023-06-16 00:59:45 +0530

    port and router in main

--- main.go
+++ main.go
@@ -2,12 +2,22 @@ package main
 import(
 	"os"
 
+	
+
 "github.com/gin-gonic/gin")
 
 func main(){
 
 
+port:=os.Getenv("PORT")
+
+if port==""{
+	port="8000"
+}
 
 
+router:=gin.New()
+
+router.Use(gin.Logger())
 	
 }
