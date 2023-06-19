081193754b398ca6c0101519e51b6bed1a0102a0
Himanshu Kumar Singh <97310760+to4to@users.noreply.github.com>
2023-06-20 00:00:27 +0530

    imports in authRoute + USer Route

--- routes/authRouter.go
+++ routes/authRouter.go
@@ -1,1 +1,17 @@
 package routes
+
+
+import (
+	 "github.com/to4to/Go-JWT-Auth/controller"
+	"github.com/gin-gonic/gin"
+)
+
+
+func AuthRoutes(incomingRoutes *gin.Engine){
+  
+incomingRoutes.POST("user/signup",controller.Signup())
+incomingRoutes.POST("user/login",controller.Login())
+
+
+
+}
--- routes/userRouter.go
+++ routes/userRouter.go
@@ -1,1 +1,9 @@
 package routes
+
+
+
+
+import("github.com/gin-gonic/gin"
+"github.com/to4to/Go-JWT-Auth/controllers"
+
+)
